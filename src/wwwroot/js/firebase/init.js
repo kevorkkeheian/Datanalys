86d7de58c01a1faa1786978b0159c8546d8b19fa
Kevork Keheian <kevork.keheian@codgram.com>
2020-09-20 19:20:38 +0300

    🔧 [Add] firebase configuration

---
+++ src/wwwroot/js/firebase/init.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+      // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+      var firebaseConfig = {
+        apiKey: "AIzaSyBrG9IcoqmHkvqzFIxI7ka-fZYL5gKbBKo",
+        authDomain: "datanalys-ml.firebaseapp.com",
+        databaseURL: "https://datanalys-ml.firebaseio.com",
+        projectId: "datanalys-ml",
+        storageBucket: "datanalys-ml.appspot.com",
+        messagingSenderId: "763086095288",
+        appId: "1:763086095288:web:41a2dbb4c54cd62b48054d",
+        measurementId: "G-7JRWRPJK86"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
